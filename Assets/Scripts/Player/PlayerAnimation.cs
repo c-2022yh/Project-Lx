e5e51c7df7acbdf35e89eb1e99014db2992601b8
c-2022yh <c_2022yh@naver.com>
2026-09-07 01:11:20 +0900

    260907 0110
    플레이어 애니메이션 구현
    대쉬는 어캐할지 고민중

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -1,16 +1,14 @@
 using UnityEngine;
 
-//플레이어의 애니메이션 출력을 처리하는 스크립트
+// 플레이어의 애니메이션 출력을 처리하는 스크립트
 public class PlayerAnimation : MonoBehaviour
 {
-    //기본 변수
     private Player player;
     private Animator animator;
 
     private static readonly int Speed = Animator.StringToHash("Speed");
-    //private static readonly int IsGrounded = Animator.StringToHash("IsGrounded");
-    //private static readonly int VerticalVelocity = Animator.StringToHash("VerticalVelocity");
-    //private static readonly int Dash = Animator.StringToHash("Dash");
+    private static readonly int IsGrounded = Animator.StringToHash("IsGrounded");
+    private static readonly int VerticalVelocity = Animator.StringToHash("VerticalVelocity");
 
     private void Awake()
     {
@@ -20,19 +18,8 @@ public class PlayerAnimation : MonoBehaviour
 
     private void Update()
     {
-        float speed = Mathf.Abs(player.rb.linearVelocity.x);
-        animator.SetFloat(Speed, speed);
-    }
-
-    private void UpdateMovementAnimation()
-    {
         animator.SetFloat(Speed, Mathf.Abs(player.rb.linearVelocity.x));
-        //animator.SetBool(IsGrounded, player.isGrounded);
-        //animator.SetFloat(VerticalVelocity, player.rb.linearVelocity.y);
-    }
-
-    public void PlayDash()
-    {
-        //animator.SetTrigger(Dash);
+        animator.SetBool(IsGrounded, player.isGrounded);
+        animator.SetFloat(VerticalVelocity, player.rb.linearVelocity.y);
     }
 }
